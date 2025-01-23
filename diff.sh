924c2b587bba8bca5bfc78f4ec213659ff5857d4
Richard Geary <richard@skylake-capital.com>
2025-01-23 16:56:55 +0800

    Improve diff.sh command

--- diff.sh
+++ diff.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+num_diffs=0
 for f in $(ls -a dotfiles/.* dotfiles/bin/*); do
   if [[ ! -f $f ]]; then continue; fi
   dest=$HOME/$(basename $f)
   if [[ ! -f $dest ]]; then continue; fi
-  echo "diff $f $dest"
-  diff $f $dest
+  if ! diff -q $f $dest >/dev/null; then
+    echo "diff "$@" $f $dest"
+    diff "$@" $f $dest
+    num_diffs=$(($num_diffs+1))
+  fi
 done
+
+echo "$num_diffs file different"
 
