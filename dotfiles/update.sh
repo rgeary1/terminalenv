5fdc955b81ac689eab6ab71d0d0573a88171947b
Richard Geary <richard@skylake-capital.com>
2025-02-14 17:37:17 +0800

    Add update.sh script

---
+++ dotfiles/update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+ 
+export SRC_URL=${SRC_URL-https://raw.githubusercontent.com/rgeary1/terminalenv/refs/heads/master}
+
+set -eu
+
+# Validation
+if [[ ! -d $HOME || ! -w $HOME ]]; then
+    echo 'No writeable $HOME'
+    exit 1
+fi
+export SRCDIR="${SRCDIR-$HOME}/.dotfiles"
+
+echo "Copying to $SRCDIR"
+mkdir -p "$SRCDIR"
+
+for f in filelist install.sh diff.sh update.sh; do
+  echo "curl -s $SRC_URL/dotfiles/$f -o $SRCDIR/$f"
+  curl -s "$SRC_URL/dotfiles/$f" -o "$SRCDIR/$f"
+done
+chmod +x "$SRCDIR/update.sh"
+chmod +x "$SRCDIR/install.sh"
+chmod +x "$SRCDIR/diff.sh"
+
+# Copy the files
+for f in $(cat $SRCDIR/filelist); do
+  dir=$(dirname "$f")
+  mkdir -p "$SRCDIR/$dir"
+  echo curl -s $SRC_URL/dotfiles/$f -o "$SRCDIR/$f"
+  curl -s $SRC_URL/dotfiles/$f -o "$SRCDIR/$f" || echo "Failed to copy $f"
+done
+
+# Install the files
+"$SRCDIR/install.sh"
+
--- install.sh
+++ install.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
- 
+
 export SRC_URL=${SRC_URL-https://raw.githubusercontent.com/rgeary1/terminalenv/refs/heads/master}
 
 set -eu
 
-# Validation
-if [[ ! -d $HOME || ! -w $HOME ]]; then
-    echo 'No writeable $HOME'
-    exit 1
-fi
-export SRCDIR="${SRCDIR-$HOME}/.dotfiles"
+DOTFILES=$HOME/.dotfiles
+mkdir -p $DOTFILES
+curl -s -L $SRC_URL/dotfiles/update.sh -o $DOTFILES/update.sh
 
-echo "Copying to $SRCDIR"
-mkdir -p "$SRCDIR"
-
-for f in filelist install.sh diff.sh; do
-  echo "curl -s $SRC_URL/dotfiles/$f -o $SRCDIR/$f"
-  curl -s "$SRC_URL/dotfiles/$f" -o "$SRCDIR/$f"
-done
-chmod +x "$SRCDIR/install.sh"
-chmod +x "$SRCDIR/diff.sh"
-
-# Copy the files
-for f in $(cat $SRCDIR/filelist); do
-  dir=$(dirname "$f")
-  mkdir -p "$SRCDIR/$dir"
-  echo curl -s $SRC_URL/dotfiles/$f -o "$SRCDIR/$f"
-  curl -s $SRC_URL/dotfiles/$f -o "$SRCDIR/$f" || echo "Failed to copy $f"
-done
-
-# Install the files
-"$SRCDIR/install.sh"
+bash $DOTFILES/update.sh
 
